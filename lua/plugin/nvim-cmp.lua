23d2b63b9b841bf57500ac1793596a0a599b5a82
Noveren <1079148276@qq.com>
2023-01-09 01:48:52 +0800

    add pylsp

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -18,8 +18,8 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert {
         -- 仅在补全窗口出现时才有效的按键映射
         ["<Esc>"]  = cmp.mapping.abort(),
-        ["<Tab>"]   = cmp.mapping.confirm({ select = true }),
+        ["<CR>"]   = cmp.mapping.confirm({ select = true }),
         ["<Up>"]   = cmp.mapping.select_prev_item(),
-        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
     },
 }
